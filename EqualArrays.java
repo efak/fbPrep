fec293fcd6d8e09bd1f0dc1f2aca1a1d4fd19959
efak <francisakowuah@gmail.com>
2021-02-04 12:56:26 -0500

    Update EqualArrays.java

--- EqualArrays.java
+++ EqualArrays.java
@@ -13,7 +13,10 @@ class EqualArrays {
 
   // Add any helper functions you may need here
   
-
+  //Treating this problem as an anagram problem since 
+  //we can reverse B multiple ways
+  
+  //This approach will be O(nlogn) due to sorting
   boolean areTheyEqual(int[] array_a, int[] array_b) {
     // Write your code here
         
@@ -25,6 +28,27 @@ class EqualArrays {
     }
     
     return true;    
+  }
+  
+  //O(N) 
+  boolean areTheyEqual(int[] array_a, int[] array_b) {
+    // Write your code here
+    
+    HashMap<Integer,Integer> map = new HashMap<>();
+
+    Integer freq;
+    for(int e: array_a){
+        freq = map.get(e);
+        map.put(e, (freq == null) ? 1 : freq + 1 );        
+    }
+
+    for(int e: array_b){
+        freq = map.get(e);
+        if( freq == null || freq == 0) return false;
+        map.put(e, freq - 1 );        
+    }
+    
+    return true;
   }
 
 }
