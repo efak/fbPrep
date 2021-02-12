0f027b3651ba8b1832a1b0af67cbeda90a9c4769
efak <francisakowuah@gmail.com>
2021-02-12 17:07:10 -0500

    Update LargestTripleProduct.java

--- LargestTripleProduct.java
+++ LargestTripleProduct.java
@@ -1,6 +1,7 @@
 public class LargestTripleProduct {
     /**
-     * Largest Triple Products You're given a list of n integers arr[0..(n-1)]. You
+     * Largest Triple Products: 
+     * You're given a list of n integers arr[0..(n-1)]. You
      * must compute a list output[0..(n-1)] such that, for each index i (between 0
      * and n-1, inclusive), output[i] is equal to the product of the three largest
      * elements out of arr[0..i] (or equal to -1 if i < 2, as arr[0..i] then
@@ -17,25 +18,31 @@ public class LargestTripleProduct {
         int n = arr.length;
         int product = 1;
         int[] output = new int[n];
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
         // populate heap with first 3 integers
         // and calculate product
         if (n >= 3) {
             for (int i = 0; i < 3; i++) {
-                maxHeap.add(arr[i]);
+                minHeap.add(arr[i]);
                 product *= arr[i];
             }
         }
+        else{
+            output[0] = -1;
+            output[1] = -1;
+            return output;
+        }
+            
         output[0] = -1;
         output[1] = -1;
         output[2] = product;
 
         for (int i = 3; i < n; i++) {
-            int peeked = maxHeap.peek();
+            int peeked = minHeap.peek();
             if (arr[i] > peeked) {
-                maxHeap.poll();
-                maxHeap.add(arr[i]);
+                minHeap.poll();
+                minHeap.add(arr[i]);
 
                 product = product / peeked;
                 product = product * arr[i];
